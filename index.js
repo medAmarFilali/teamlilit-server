9c528f3a34ad7d003b67832cff163ea6474d3bb5
Filali Med Amar <medamarfilali@gmail.com>
2022-01-20 15:13:20 +0100

    Changed the socketio origin

--- index.js
+++ index.js
@@ -1,15 +1,22 @@
 const express = require("express");
+const http = require("http");
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const morgan = require("morgan");
-const userRouter = require("./routes/userRouter");
-
 const app = express();
-
+const server = http.createServer(app);
 dotenv.config();
+const io = require("socket.io")(server, {
+  cors: {
+    origin: [process.env.CLIENT_URL],
+    methods: ["GET", "POST"],
+  },
+});
+
+const userRouter = require("./routes/userRouter");
 
 app.use(morgan("dev"));
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
@@ -23,13 +30,29 @@ app.use(
 );
 app.use("/user", userRouter);
 
+io.on("connection", (socket) => {
+  socket.emit("me", socket.id);
+
+  socket.on("disconnect", () => {
+    socket.broadcast.emit("callEnded");
+  });
+
+  socket.on("callUser", ({ userToCall, signalData, from, name }) => {
+    io.to(userToCall).emit("callUser", { signal: signalData, from, name });
+  });
+
+  socket.on("answerCall", (data) => {
+    io.to(data.to).emit("callAccepted", data.signal);
+  });
+});
+
 mongoose
   .connect(process.env.DB_CONNECTION_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
   .then(
-    app.listen(process.env.APP_PORT, () => {
+    server.listen(process.env.APP_PORT, () => {
       console.log(`Listening to http://localhost:${process.env.APP_PORT}`);
     })
   )
