573ab62a57876ada2b93602dd5ce5d8bdd99f749
Filali Med Amar <medamarfilali@gmail.com>
2022-01-28 13:16:15 +0100

    Changed cors to all

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const server = http.createServer(app);
 dotenv.config();
 const io = require("socket.io")(server, {
   cors: {
-    origin: process.env.CLIENT_URL,
+    origin: "*",
     methods: ["GET", "POST"],
     allowHeaders: ["Access-Control-Allow-Origin"],
     credentials: true,
@@ -28,7 +28,7 @@ app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cookieParser());
 app.use(
   cors({
-    origin: process.env.CLIENT_URL,
+    origin: "*",
     credentials: true,
   })
 );
