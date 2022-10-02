f2a8393c3700d44664ba0bf54f3bcb1b6711c24e
Filali Med Amar <medamarfilali@gmail.com>
2022-10-02 13:54:51 +0100

    Set httponly to false

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -82,7 +82,7 @@ const loginUser = async (req, res, next) => {
 
         res.cookie("access_token", token, {
           // expires: new Date(Date.now() + 9999999),
-          httpOnly: true,
+          httpOnly: false,
           sameSite: "none",
           secure: process.env.NODE_ENV === "production" ? true : false,
           domain: process.env.CLIENT_DOMAIN
